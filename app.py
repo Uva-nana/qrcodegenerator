7f3b14000e56317c5cb34c258832fee0108fb56a
Uva-nana <uvananaj1l@gmail.com>
2025-05-12 11:19:40 +0530

    Updated and tried with changes where url check(http:// should present)

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 import qrcode
-import os #inbuilt libraries
+import os
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Needed for flashing messages
 
 @app.route('/', methods=['GET', 'POST'])
 def showqr():
     if request.method == 'POST':
-        url = request.form['url']
+        url = request.form['url'].strip()
+        # Validate: must start with http:// or https://
+        if not (url.startswith("http://") or url.startswith("https://")):
+            flash("Invalid URL! Please include http:// or https://")
+            return render_template('qrform.html')
+
+        # Generate QR code
         img = qrcode.make(url)
         path = os.path.join('static', 'qr.png')
         img.save(path)
